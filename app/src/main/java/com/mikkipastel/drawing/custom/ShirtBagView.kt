a4bedaea5ab07e6bcbdb78801675c91f4e917b77
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2021-10-08 13:14:29 +0700

    fix: change style to stroke and move init path at global variablr for ShirtBagView

--- app/src/main/java/com/mikkipastel/drawing/custom/ShirtBagView.kt
+++ app/src/main/java/com/mikkipastel/drawing/custom/ShirtBagView.kt
@@ -14,25 +14,27 @@ class ShirtBagView @JvmOverloads constructor(
     private val paint = Paint().apply {
         isAntiAlias = true
         color = Color.BLACK
+        style = Paint.Style.STROKE
+    }
+
+    private val path = Path().apply {
+        moveTo(20f, 20f)
+        lineTo(40f, 20f)
+        arcTo(
+            RectF(40f, 5f, 70f, 35f),
+            180f,
+            -180f,
+            false
+        )
+        lineTo(90f, 20f)
+        lineTo(90f, 90f)
+        lineTo(20f, 90f)
+        close()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        val path = Path().apply {
-            moveTo(20f, 20f)
-            lineTo(40f, 20f)
-            arcTo(
-                RectF(40f, 5f, 70f, 35f),
-                180f,
-                -180f,
-                false
-            )
-            lineTo(90f, 20f)
-            lineTo(90f, 90f)
-            lineTo(20f, 90f)
-            close()
-        }
         canvas?.drawPath(path, paint)
     }
 }
